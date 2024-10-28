3234bb40f496b0469cad9a2af3ca2b454135dd94
Sameer Mehta <sameer@smehta.ca>
2024-10-28 07:28:13 -0400

    Setup NextJS, DaisyUI and Jest

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# wiki-scholar
+# Wiki Scholar App
+
+## Getting Started
+
+First, run the development server:
+
+```bash
+npm install
+npm run dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the app.
---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    daisyui: {
+      themes: ["light", "dark"],
+    },
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+};
+export default config;
